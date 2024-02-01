27aa08e2b53970ad4ba95a7eef0d37e72b172fa0
JAMES MBUVI <completed assignment>
2024-02-01 16:26:26 +0300

    completed assignment

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,19 @@
 // Code Keypad Component Here
+import React from "react";
+
+
+function handleClick () {
+    console.log("Entering password...")
+}
 
 function Keypad (){
     return (
-        <div></div>
+        <div>
+<input 
+type="password"
+onChange={handleClick}
+/>
+        </div>
     )
 }
 
